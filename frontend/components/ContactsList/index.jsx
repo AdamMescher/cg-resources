fded2a67aee59a7a643039236a1ca48b02af7b7e
Adam Mescher <admescher@gmail.com>
2021-11-08 15:26:05 -0700

    Update ContactsList textAlign and padding

--- frontend/components/ContactsList/index.jsx
+++ frontend/components/ContactsList/index.jsx
@@ -1,4 +1,12 @@
-import { Box, HStack, Icon, Link, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Center,
+  HStack,
+  VStack,
+  Icon,
+  Link,
+  Text,
+} from '@chakra-ui/react';
 import { EmailIcon, PhoneIcon } from '@chakra-ui/icons';
 import { MdCake } from 'react-icons/md';
 import personModal from '../PersonModal';
@@ -7,18 +15,20 @@ import formatPhoneNumber from '../../utils/formatPhoneNumber';
 const ContactsList = ({ people }) => {
   return people.map((person) => (
     <Box key={person._id} mt={3}>
-      <Text fontSize={24}>{`${person.firstName} ${person.lastName}`}</Text>
-      <HStack>
+      <Text
+        fontSize={24}
+        textAlign='center'>{`${person.firstName} ${person.lastName}`}</Text>
+      <HStack ml={10}>
         <PhoneIcon />
         <Link href={`tel:${person.phone}`}>
           {formatPhoneNumber(person.phone)}
         </Link>
       </HStack>
-      <HStack>
+      <HStack ml={10}>
         <EmailIcon />
         <Link href={`mailto:${person.email}`}>{person.email}</Link>
       </HStack>
-      <HStack>
+      <HStack ml={10}>
         <Icon as={MdCake} />
         <Text>{person.birthday}</Text>
       </HStack>
