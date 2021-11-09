5afff5c210a9cd3e0f69df6d98fef3e5e67bd7d9
Adam Mescher <admescher@gmail.com>
2021-11-09 13:00:30 -0700

    Update Head with meta tags

--- frontend/components/NextHead/index.jsx
+++ frontend/components/NextHead/index.jsx
@@ -3,8 +3,14 @@ import Head from 'next/head';
 const NextHead = () => {
   return (
     <Head>
-      <title>Hans CG Resources</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <meta charSet='utf-8' />
+      <title>Hans CG Resources</title>
+      <meta
+        name='description'
+        content='Resources for Hans CG at The Heights Church in Denver, CO'
+      />
+      <meta name='author' content='Adam Mescher' />
     </Head>
   );
 };
